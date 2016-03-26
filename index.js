6bc6638d404e98edb9d91c98d6539dc9b275f632
Go7hic <gtfx0209@qq.com>
2016-03-26 20:42:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+export default leftpad
+function leftpad (str, len, ch) {
+  str = String(str)
+  len = len - str.length
+  if (len < 1) return str
+  if (!ch && ch !== 0) ch = ' '
+  return ch.repeat(len) + str
+}
